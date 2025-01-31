b42b4296e8c2dc38289f2772056e477ab492d775
Duy Quang <quangduytc1709@gmail.com>
2025-01-31 11:36:08 +0700

    Add Spawing Block, Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject block;
+    public float maxX;
+    public Transform spawnPoint;
+    public float spawnRate;
+
+    private bool gameStarted = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && !gameStarted) { 
+            StartSpawning();
+
+            gameStarted = true;
+        }
+    }
+
+    private void StartSpawning() {
+        InvokeRepeating("SpawnBlock", 0.5f, spawnRate);
+    }
+
+    private void SpawnBlock() { 
+        Vector3 spawnPos = spawnPoint.position;
+
+        spawnPos.x = Random.Range(-maxX, maxX);
+
+        Instantiate(block, spawnPos, Quaternion.identity);
     }
 }
